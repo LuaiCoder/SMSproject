1dc6593561684e11d7ed45daee5870797ba376fb
LuaiCoder <48597403+LuaiCoder@users.noreply.github.com>
2019-07-24 09:03:25 +0100

    Create PhoneCompany.java

---
+++ PhoneCompany.java
@@ -0,0 +1,9 @@
+public interface PhoneCompany {
+
+    /*
+     * This is the method you should call to submit a message to a phone company.
+     */
+
+    void deliverSmsMessage(Message message);
+
+}
